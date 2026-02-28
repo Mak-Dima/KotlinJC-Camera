8ed6d44253df5de8519027c4a25dd377d2f3d6fc
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-02-27 18:03:09 -0600

    MainScreen composable added.

---
+++ app/src/main/java/com/example/kotlinjc_camera/MainScreen.kt
@@ -0,0 +1,33 @@
+package com.example.kotlinjc_camera
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+
+@Composable
+fun MainScreen(innerPadding: PaddingValues) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Take a picture")
+        }
+    }
+}
+
+@PreviewScreenSizes
+@Composable
+fun MainScreenPreview() {
+    val innerPadding = PaddingValues()
+    MainScreen(innerPadding)
+}
